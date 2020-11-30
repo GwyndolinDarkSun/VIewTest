c41cf23fa1979fbceeabae0a84b1a74b1820c3cd
Dzy <3200519145@qq.com>
2020-11-30 13:01:54 +0800

    线程池

--- src/BF/P2/Bank.java
+++ src/BF/P2/Bank.java
@@ -1,30 +1,52 @@
 package BF.P2;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
-    private final double[] accounts;
+    private final double[] accounts;//用户
+    private Lock bankLock;//锁对象
+    private Condition sufficientFunds;//条件对象
+
     public Bank(int n,double initialBalance){
         accounts = new double[n];
         Arrays.fill(accounts,initialBalance);
+        bankLock = new ReentrantLock();
+        sufficientFunds = bankLock.newCondition();
     }
 
-    public void transfer(int from,int to,double amount){
-        if(accounts[from] < amount){
-            return;
+    public void transfer(int from,int to,double amount) throws InterruptedException{
+        bankLock.lock();
+        try{
+            while(accounts[from] < amount){
+                sufficientFunds.await();
+            }//如果钱不够那就一直等待
+            System.out.println(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" $10.2f from %d",amount,from,to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2f%n",getTotalBalance());
+            sufficientFunds.signalAll();
         }
-        System.out.println(Thread.currentThread());
-        accounts[from] -= amount;
-        System.out.printf(" $10.2f from %d",amount,from,to);
-        accounts[to] += amount;
-        System.out.printf(" Total Balance: %10.2f%n",getTotalBalance());
+        finally {
+            bankLock.unlock();
+        }
+
     }
     public double getTotalBalance(){
-        double sum = 0;
-        for (double a : accounts){
-            sum += a;
+        bankLock.lock();
+        try{
+            double sum = 0;
+            for (double a : accounts){
+                sum += a;
+            }
+            return sum;
         }
-        return sum;
+        finally {
+            bankLock.unlock();
+        }
     }
     public int size(){
         return accounts.length;
